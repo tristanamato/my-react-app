9b137909e24266e530e3dddb6bdd459465d03833
Tristan AMATO <tristan.amato@edu.esce.fr>
2023-10-12 10:16:41 +0200

    undefined mew

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,25 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-            <figcaption>Bulbasaur</figcaption>           
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+        <figcaption>{pokemon.name}</figcaption>        
         </figure>
     );
 }
+
 
 export default PokemonCard;
